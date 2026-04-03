77268b10c3dacdf111e1ecc15f5995308dfa998e
Soubhagya <soubhagyarbiswal@gmail.com>
2026-04-03 12:30:09 +0530

    Update board.c

--- board.c
+++ board.c
@@ -13,16 +13,16 @@ int oppdir(int direction){
 }
 
 int samepos(Position a, Position b){
-    return ((a.x == b.x) && (a.y == b.y));
+    return (a.x == b.x && a.y == b.y);
 }
 
 void createboard(Board *board){
-board->move=0;
-board->first.x=-1;
-board->first.y=-1;
-board->result=open;
-setcells(board);
-legalmoves(board);
+    board->move=0;
+    board->first.x=-1;
+    board->first.y=-1;
+    board->result=open;
+    setcells(board);
+    legalmoves(board);
 }
 
 void setcells(Board *board){
@@ -114,7 +114,7 @@ int offboard(Position position){
     if (position.x>23 || position.x<0 || position.y>23 || position.y<0 ){
         return 1;
     }
-else if ((position.x == 0 && position.y == 23) || (position.x == 23 && position.y == 0) || (position.x == 0 && position.y == 0) || (position.x == 23 && position.y == 23)){
+    else if ((position.x == 0 && position.y == 23) || (position.x == 23 && position.y == 0) || (position.x == 0 && position.y == 0) || (position.x == 23 && position.y == 23)){
         return 1;
     }
     else return 0;
@@ -150,29 +150,120 @@ int linkage(Board *board, Position here, int direction){
     Cell *chere, *cthere;
     Position check;
     Cell *ccheck;
+
     there.x = here.x + diff[direction][0];
     there.y = here.y + diff[direction][1];
+
     if (offboard(there)){
         return 0;
     }
+
     else{
         chere = viewcell(board,here);
         cthere = viewcell(board,there);
+
     if (cthere->colour != chere->colour){
         return 0;
         }
+    
     else{
         for (int i=0; i<24; i++){
             for (int j=0; j<24; j++){
-                check.x = i;
-                check.y = j;
+                check.x = j;
+                check.y = i;
+
                 if (offboard(check)){
                     continue;
+                    }
+                else{
+                    ccheck = viewcell(board, check);
+                    for (int k=0; k<8; k++){
+                        if (ccheck->links & (1<<k)){
+                            if (crosslinkage(here,direction,check,k)){
+                                return 0;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+return 1;
 }
+
+int crosslinkage(Position P1, int D1, Position P2, int D2){
+    Position E1,E2;
+    int mx1,Mx1,my1,My1;
+    int mx2,Mx2,my2,My2;
+    long long dx1,dx2,dy1,dy2;
+    long long dx12,dy12;
+    long long c1,c2;
+
+    E1.x = P1.x + diff[D1][0];
+    E1.y = P1.y + diff[D1][1];  
+    E2.x = P2.x + diff[D2][0];
+    E2.y = P2.y + diff[D2][1];
+    
+    if ((samepos(P1,P2) && D1==D2) || (samepos(P1,E2) && samepos(E1,P2))|| samepos(P1,P2) || samepos(P1,E2) || samepos(E1,P2) || samepos(E1,E2)){
+        return 0;
+    }
+
+    if (P1.x<E1.x){
+        mx1=P1.x;
+        Mx1=E1.x;
+    }
+    else{
+        mx1=E1.x;
+        Mx1=P1.x;
+    }
+    if (P1.y<E1.y){
+        my1=P1.y;
+        My1=E1.y;
+    }
+    else{
+        my1=E1.y;
+        My1=P1.y;
+    }
+    if (P2.x<E2.x){
+        mx2=P2.x;
+        Mx2=E2.x;
+    }
+    else{
+        mx2=E2.x;
+        Mx2=P2.x;
+    }
+    if (P2.y<E2.y){
+        my2=P2.y;   
+        My2=E2.y;
+    }
+    else{
+        my2=E2.y;
+        My2=P2.y;
+    }
+
+if (Mx1<mx2 || Mx2<mx1 || My1<my2 || My2<my1){
+    return 0;
+    }
+
+dx1=E1.x-P1.x;
+dy1=E1.y-P1.y;
+dx2=E2.x-P2.x;
+dy2=E2.y-P2.y;
+dx12=P2.x-P1.x;
+dy12=P2.y-P1.y;
+c1 = (dx1*dy12)-(dy1*dx12);
+c2 = (dx1*(E2.y-P1.y))-(dy1*(E2.x-P1.x));
+if((c1>0 && c2>0) || (c1<0 && c2<0)){
+    return 0;
+}
+dx12=P1.x-P2.x;
+dy12=P1.y-P2.y;
+c1 = (dx2*dy12)-(dy2*dx12);
+c2 = (dx2*(E1.y-P2.y))-(dy2*(E1.x-P2.x));
+if((c1>0 && c2>0) || (c1<0 && c2<0)){
+    return 0;
+    }
 return 1;
 }
 
@@ -180,6 +271,7 @@ void peg(Board* board, int player, Position positon){
     Cell *cell;
     Position target;
     Cell *ctarget;
+
     cell = viewcell(board,positon);
     if (player == red){
         cell->colour = redpeg;
@@ -187,17 +279,20 @@ void peg(Board* board, int player, Position positon){
     else if (player == blue){
         cell->colour = bluepeg;
     }
+
     for (int i =0; i<8; i++){
         target.x = positon.x + diff[i][0];
         target.y = positon.y + diff[i][1];
+
         if (offboard(target)){
         continue;
         }
-ctarget = viewcell(board,target);
+    ctarget = viewcell(board,target);
     if (ctarget->colour == cell->colour){
         if (linkage(board,positon,i)){
             cell->links |= (1<<i);
         ctarget->links |= (1<<oppdir(i));
+
     printf("BINGO!\n");
     printf("You have created a link! : %c%d <==> %c%d\n", 'a'+positon.x,positon.y+1,'a'+target.x, target.y+1);
         }
@@ -207,23 +302,29 @@ ctarget = viewcell(board,target);
             }
         }
     }
-update(board,positon,player);
+update(board,player);
 }
 
-void update(Board *board, Position position, int player){
+void update(Board *board, int player){
     Cell* cell;
     for(int i=0; i<24; i++){
         for(int j=0; j<24; j++){
             board->cells[i][j].start[player]=0;
             board->cells[i][j].end[player]=0;
         }
-    }
     for (int i=0; i<24; i++){
         for (int j=0; j<24; j++){
             Position p;
             p.x=j;
             p.y=i;
             cell = &board->cells[i][j];
+            Peg buffer;
+            if (player == red){
+                buffer = redpeg;
+            }
+            else if (player == blue){
+                buffer = bluepeg;
+            }
             if (player == red && i==0 && j>0 && j<23){
             cell->start[player]=1;
             }
@@ -238,6 +339,7 @@ void update(Board *board, Position position, int player){
                 }
             }
         }
+    }
     int flag=1,a=0;
     while (flag && a<10000){
         flag=0;
@@ -251,10 +353,10 @@ void update(Board *board, Position position, int player){
                         if(cell->links & (1<<k)){
                             Position next = cell->neigbours[k];
                             if (next.x >=0 && next.y>=0){
-                                Cell *cnext= &board->cells[next.y][next.x];
+                                Cell *cnext = &board->cells[next.y][next.x];
                                 if (!cnext->start[player]){
                                     cnext->start[player]=1;
-                                    flag =1;
+                                    flag=1;
                                 }
                             }
                         }
@@ -275,10 +377,10 @@ void update(Board *board, Position position, int player){
                         if(cell->links & (1<<k)){
                             Position next = cell->neigbours[k];
                             if (next.x >=0 && next.y>=0){
-                                Cell *cnext =&board->cells[next.y][next.x];
+                                Cell *cnext = &board->cells[next.y][next.x];
                                 if (!cnext->end[player]){
                                     cnext->end[player]=1;
-                                    flag= 1;
+                                    flag=1;
                                 }
                             }
                         }
@@ -353,15 +455,18 @@ void print(Board *board){
     for(int i=0; i<24; i++){
         printf("\033[31m%c\033[0m ",'a'+i);
     }
+
     printf("\n    ");
     printf("-");
     for(int i=0; i<24; i++){
         printf("--");
     }
     printf("-     \n");
+
     for (int i=0; i<24; i++){
         printf(" \033[34m%2d\033[0m |",i+1);
         for (int j=0; j<24; j++){
+
             position.x = j;
             position.y = i;
             if (offboard(position)){
@@ -388,6 +493,7 @@ for (int i=0; i<24; i++){
     }  
 printf("-\n");
 printf("     ");
+
 for (int i=0; i<24; i++){
     printf("\033[31m%c\033[0m ",'a'+i);
     }
@@ -424,4 +530,79 @@ int checkaction(Action *list, int action){
         }
     }
 return 0;
+}
+
+void printlinks(Board* board){
+    Position position;
+    Cell *cell;
+    int rc=0,bc=0;
+
+    printf("\n");
+    printf("=============\n");
+    printf("Current LInks\n");
+    printf("=============\n");
+
+    printf("\n");
+    printf("RED LINKS:\n");
+    for (int i=0; i<24; i++){
+        for (int j=0; j<24; j++){
+            position.x = j;
+            position.y = i;
+            if (offboard(position)){
+                continue;
+            }
+            cell = viewcell(board,position);
+            if (cell->colour == redpeg){
+                for (int k=0; k<8; k++){
+                    if (cell->links & (1<<k)){
+                        Position target = cell->neigbours[k];
+                        if (target.x >= 0 && target.y >=0){
+                            if (position.y < target.y || (position.y == target.y && position.x <target.x)){
+                                printf("  %c%d <==> %c%d\n",'a'+position.x,position.y+1,'a'+target.x,target.y+1);
+                                rc++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (rc==0){
+        printf(" No links yet :( \n");
+    }
+    else{
+        printf(" Total: %d links\n",rc);
+    }
+    printf("\n");
+    printf("BLUE LINKS:\n");
+    for (int i=0; i<24; i++){
+        for (int j=0; j<24; j++){
+            position.x = j;
+            position.y = i;
+            if (offboard(position)){
+                continue;
+            }
+            cell = viewcell(board,position);
+            if (cell->colour == bluepeg){
+                for (int k=0; k<8; k++){
+                    if (cell->links & (1<<k)){
+                        Position target = cell->neigbours[k];
+                        if (target.x >= 0 && target.y >=0){
+                            if (position.y < target.y || (position.y == target.y && position.x <target.x)){
+                                printf("  %c%d <==> %c%d\n",'a'+position.x,position.y+1,'a'+target.x,target.y+1);
+                                bc++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (bc==0){
+        printf(" No links yet :( \n");
+    }
+    else{
+        printf(" Total: %d links\n",bc);
+    }
+printf("\n");
 }
